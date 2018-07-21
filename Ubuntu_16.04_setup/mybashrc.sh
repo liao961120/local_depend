05671dc532d0d6b2606035f871ce7d5f9a917fb9
liao@nb <liaomovie2@gmail.com>
2018-07-21 19:21:32 +0800

    up

--- Ubuntu_16.04_setup/mybashrc.sh
+++ Ubuntu_16.04_setup/mybashrc.sh
@@ -1,16 +1,15 @@
 alias app_link='xdg-open /usr/share/applications/'
-alias timecal='bash ~/sh_program/time_cal.sh'
 alias dict='goldendict'
 alias mylinux='vim ~/local_depend/Ubuntu_16.04_setup/mylinux.Rmd'
-alias render_mylinux='bash ~/sh_program/render_mylinux.sh'
 alias r='Rscript -e' #Note capital 'R' is for running R in bash
-alias render_rmdbk='bash ~/sh_program/render_rmarkdown_book.sh'
+alias render_rmdbk='~/bin/render_rmarkdown_book'
 alias rmdbk='vim ~/local_depend/GitHubPage/my_R_book/Rmarkdown_features/rmd_features.rmd'
-alias new_rmd='bash ~/sh_program/append_head_rmd.sh'
+alias new_rmd='~/bin/append_head_rmd'
 
 export RSTUDIO_PANDOC=/usr/lib/rstudio/bin/pandoc
 PS1="\[\033[1;32m\]\u@nb:\[\033[1;34m\]\w\[\033[00m\]$ "
 export PS1
+export PATH=~/bin:"$PATH"
 
 # Define variables for SSH remote pc.
 ### Reads IP from git repo ~/local_depend/hostname/hostname.txt ###
@@ -21,9 +20,10 @@ rm_ip_b=`bash ~/ip2ipb.sh $rm_ip` #see 'ip2ipb.sh for detail'
 ssh_pc="liao961120@$rm_ip"  #used as 'ssh $ssh_pc'
 
 
+# Backup ~/sh_program
+cp -r ~/bin ~/local_depend/Ubuntu_16.04_setup/sh_program_bak
+
 # Backup self-defined cmd in git repo (prt lines after 'line 120')
 tail -n +120  ~/.bashrc > ~/local_depend/Ubuntu_16.04_setup/mybashrc.sh
 alias bak_rc='tail -n +120  ~/.bashrc > ~/local_depend/Ubuntu_16.04_setup/mybashrc_man.sh; echo -e "\n\n\n#### Backup Time\t$(date) ####" >> ~/local_depend/Ubuntu_16.04_setup/mybashrc_man.sh'
-# Backup ~/sh_program
-cp -r ~/sh_program ~/local_depend/Ubuntu_16.04_setup/sh_program_bak
 
