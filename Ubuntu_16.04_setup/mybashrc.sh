4819ff5a2f941033fbdf68a62e62d384d36221c7
liao <liaomovie2@gmail.com>
2018-11-07 15:01:22 +0800

    up

--- Ubuntu_16.04_setup/mybashrc.sh
+++ Ubuntu_16.04_setup/mybashrc.sh
@@ -5,11 +5,15 @@ alias render_rmdbk='~/bin/render_rmarkdown_book'
 alias rmdbk='code ~/local_depend/GitHubPage/my_R_book/Rmarkdown_features/rmd_features.rmd'
 alias new_rmd='~/bin/append_head_rmd'
 alias R='R --no-save -q'
+alias pyvir='source bin/activate'
+alias xampp='sudo /opt/lampp/manager-linux-x64.run'
 
 export RSTUDIO_PANDOC=/usr/lib/rstudio/bin/pandoc
 PS1="\[\033[1;32m\]\u@nb:\[\033[1;34m\]\w\[\033[00m\]$ "
 export PS1
 export PATH=~/bin:"$PATH"  #Add ~/bin to PATH so custom shell programs can be found
+export GEM_HOME=$HOME/gems
+export PATH=$HOME/gems/bin:$PATH
 
 # Define variables for SSH remote pc.
 ### Reads IP from git repo ~/local_depend/hostname/hostname.txt ###
