221c97af6ddac2f8b0260ec476a0b9ffc0d5de25
liao <liao961120@gmail.com>
2019-07-22 19:33:38 +0800

    update

--- Ubuntu_setup/mybashrc.sh
+++ Ubuntu_setup/mybashrc.sh
@@ -7,14 +7,21 @@ alias mypython='code ~/private_repo/python/python_notes.Rmd'
 alias new_rmd='~/bin/append_head_rmd'
 alias R='R --no-save -q'
 alias r="radian"
-alias pyvir='source bin/activate'
+#alias pyvir='cd ~/jupyter; source bin/activate; cd -'
 alias xampp='sudo /opt/lampp/manager-linux-x64.run'
-alias serve='chromium-browser "http://127.0.0.1:4000"; bundle exec jekyll serve --drafts'
+alias serve_j='chromium-browser "http://127.0.0.1:4000"; bundle exec jekyll serve --drafts'
+alias ipython='ipython --no-autoindent'
+alias serve='chromium-browser "http://0.0.0.0:8000/"; python3 -m http.server'
 alias micro='~/micro'
 alias bitly='chromium-browser --incognito https://bitly.com &'
 alias pdf_resize='java -jar Desktop/briss-0.9/briss-0.9.jar'
 alias gephi='~/Desktop/SNA/gephi-0.9.2/bin/gephi'
-
+alias lopessh='ssh yongfu@140.112.147.125'
+alias pandoc='/usr/lib/rstudio/bin/pandoc/pandoc'
+alias loderunner='bash ~/Desktop/LODERUNN/LODERUNN'
+alias slp='systemctl suspend'
+alias psudow='python3 ~/Desktop/psudoword_gen.py'
+alias open='xdg-open'
 
 export RSTUDIO_PANDOC=/usr/lib/rstudio/bin/pandoc
 PS1="\[\033[1;32m\]\u@nb:\[\033[1;34m\]\w\[\033[00m\]$ "
@@ -22,6 +29,8 @@ export PS1
 export PATH=~/bin:"$PATH"  #Add ~/bin to PATH so custom shell programs can be found
 export GEM_HOME=$HOME/gems
 export PATH=$HOME/gems/bin:$PATH
+export FLASK_APP=application.py
+export FLASK_DEBUG=1
 
 
 
@@ -50,6 +59,27 @@ function timer(){
     echo -ne "$(date -u --date @$((`date +%s` - $date1)) +%H:%M:%S)\r"; 
     sleep 0.1
    done
+}
+:
+
+# pyvir
+function pyvir(){
+  if [[ $1 == '3.6' ]]; then
+    dir='/home/liao/jupyter'
+  else
+    dir='/home/liao/py3.7'
+  fi
+  
+  ori_dir=`pwd`
+  cd $dir
+  source bin/activate
+  cd $ori_dir
+}
+
+# bris
+#java -jar briss-0.9.jar xx.pdf
+function briss(){
+  java -jar ~/Desktop/briss-0.9/briss-0.9.jar ${1}
 }
 
 
